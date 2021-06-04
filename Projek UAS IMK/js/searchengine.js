67c25541265d352a92f7ea3a667a26d9c9830c59
Muhammad Rizky Cavendio <72818583+Rizkycavendio@users.noreply.github.com>
2021-06-05 01:14:23 +0700

    Add files via upload
    Ini semua kode baik HTML, CSS, JavaScript dan juga semua kelengkapan lainnya.

---
+++ Projek UAS IMK/js/searchengine.js
@@ -0,0 +1,62 @@
+function openPage(){
+    var x = document.getElementById("search").value;
+    alert('Kategori barang tersedia, klik OK');
+
+    if(x === "fashion pria"){
+        window.open("/Projek%20UAS%20IMK/fashion_pria.html");
+    }
+    if(x === "fashion wanita"){
+        window.open("/Projek%20UAS%20IMK/fashion_wanita.html");
+    }
+    if(x === "elektronik"){
+        window.open("/Projek%20UAS%20IMK/elektronik.html");
+    }
+    if(x === "alat olahraga"){
+        window.open("/Projek%20UAS%20IMK/alat_olahraga.html");
+    }
+    if(x === "peralatan rumah"){
+        window.open("/Projek%20UAS%20IMK/perkakas.html");
+    }
+    if(x === "perkakas"){
+        window.open("/Projek%20UAS%20IMK/perkakas.html");
+    }
+    if(x === "buku"){
+        window.open("/Projek%20UAS%20IMK/buku.html");
+    }
+    if(x === "jam tangan"){
+        window.open("/Projek%20UAS%20IMK/detail_jam.html");
+    }
+    if(x === "handphone"){
+        window.open("/Projek%20UAS%20IMK/detail_handphone.html");
+    }
+    if(x === "mukena"){
+        window.open("/Projek%20UAS%20IMK/detail_mukena.html");
+    }
+    if(x === "kamera"){
+        window.open("/Projek%20UAS%20IMK/detail_kamera.html");
+    }
+    if(x === "baju"){
+        window.open("/Projek%20UAS%20IMK/detail_baju.html");
+    }
+    if(x === "piring"){
+        window.open("/Projek%20UAS%20IMK/detail_piring.html");
+    }
+    if(x === "sapu"){
+        window.open("/Projek%20UAS%20IMK/detail_sapu.html");
+    }
+    if(x === "sepeda"){
+        window.open("/Projek%20UAS%20IMK/detail_sepeda.html");
+    }
+    if(x === "kerudung"){
+        window.open("/Projek%20UAS%20IMK/detail_kerudung.html");
+    }
+    if(x === "obeng"){
+        window.open("/Projek%20UAS%20IMK/detail_obeng.html");
+    }
+    if(x === "palu"){
+        window.open("/Projek%20UAS%20IMK/detail_palu.html");
+    }
+    if(x === "laptop"){
+        window.open("/Projek%20UAS%20IMK/detail_laptop.html");
+    }
+}
